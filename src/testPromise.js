ec34cd6fe6915952fbf6792b7e8621b44b7be59d
Sushant <sushantbhalla@ymail.com>
2020-03-09 14:08:31 +0530

    Further revisions and fixing some gotchas

--- src/testPromise.js
+++ src/testPromise.js
@@ -2,40 +2,38 @@
 
 class TestPromise{
 
-    status='PENDING';
+    status='PENDING'; //PENDING,FULFILLED,REJECTED
     value;
     reason;
 
-    onResolveCallbacks = new Array();
-    onRejectCallbacks = new Array();
+    onFulfilledCallbacks = new Array();
+    onRejectedCallbacks = new Array();
     registeredPromises = new Array();
 
     //where f is a function with 2 args : resolve/reject
     //def. of how resolve/reject works will be provided from client
-
-    //ToDo:: need to check the further syntax of defining it.
+    //ToDo:: need to confirm the syntax here,should be inline & also the constructor body if reqd.
     constructor(f){}
 
     //ToDo:: to make it private so that it can't be called directly
     resolve(val){
-      //if the promise is already resolved/rejected & we have the corresponding value & reason also set
+      //if the promise is already fulfilled/rejected & we have the corresponding value & reason also set
       //ignore this invocation
-      if( (this.status=='RESOLVED' && this.value) || (this.status=='REJECTED' && this.reason)) return;
-      
-      if (this===val) throw TypeError; //(From the Spec)
+      if( (this.status=='FULFILLED' && this.value) || (this.status=='REJECTED' && this.reason)) return;
+      if (val===this) return reject(TypeError); //(From the Spec)
 
       //else set the status and value accordingly & continue for it's own reg. callbacks
-      this.status='RESOLVED';
+      this.status='FULFILLED';
       this.value = val;
 
-      for(let i=0; i<onResolveCallbacks.length(); i++){
+      for(let i=0; i<onFulfilledCallbacks.length(); i++){
           
-          let currentResolveCallback = onResolveCallbacks[i];
+          let currentFulfillCallback = onFulfilledCallbacks[i];
           let correspondingPromise = registeredPromises[i];
           
-          if(!currentResolveCallback || typeof currentResolveCallback != 'function') {
-            correspondingPromise(val,null);// <-- resolve the corresponding promise with the value directly
-            return;
+          if(!currentFulfillCallback || typeof currentFulfillCallback != 'function') {
+            correspondingPromise(val,null);// <-- fulfill/resolve the corresponding promise with the value directly
+            continue;
           };
           
           try{
@@ -43,16 +41,17 @@ class TestPromise{
             //ToDo:: to wait until execution stack is empty!!
             //Invoke the callback with the val param received
             
-            let result = currentResolveCallback(val);
+            let result = currentFulfillCallback(val);
            
             if(result instanceof TestPromise){ // i.e. a promise itself
 
                 // From the Spec: adopt it's state
-                // But in reality, the only function we can call is then() on it!
+                // But in reality, the only function we can call on any promise is then() nnly
                 // so, we attach our registered task as a callback to it's then method once that completes
                 
-                // this is DIFFERENT from our understadning till now i.e. even if our rejectCallback gets called successfully
+                //Q::this is DIFFERENT from our understadning till now i.e. even if our rejectedCallback gets called successfully
                // we still resolve the promise, but in this scenario, we are rejecting it!
+               
                // ToDo:: Thus, is the syntax correct? because our then model is based on different understanding
                // or should it be like result.then((v) => correspondingPromise(v,null),null).then(null,(r) => correspondingPromise(null,r)) ???
                result.then((v) => correspondingPromise(v,null),
@@ -65,8 +64,8 @@ class TestPromise{
                     let thenProperty = result.then;
 
                     if(typeof thenProperty == 'function'){
-                       // Q: to call it with it's this==result
-                       // Q: to call it with 1st arg: resolvePromise & 2nd arg: rejectPromise ?
+                       // ToDo: to call it with it's this=result,wlthough I believe it should be fine since by def. of it ,it is result.then 
+                       // to call it with 1st arg: resolvePromise & 2nd arg: rejectPromise
                        // i.e. then(resolvePromise, rejectPromise)
                        // such that if resolvePromise is called with value y, we resolve the corresponding promise with value y
                        // if rejectPromise is called with reason r, we reject the promise with r
@@ -74,26 +73,40 @@ class TestPromise{
                        let invocationCnt = 0;
                        const resolvePromise = (y) => {
                          if(invocationCnt++ > 0) return; //oncifying attempt using closure : NEEDS to be tested.
-                         correspondingPromise(y,null);
-                       }
+                         try{
+                            correspondingPromise(y,null);
+                         }catch(error){
+                           correspondingPromise(null,error);
+                         }
+                        }
 
-                       // this is DIFFERENT from our understadning till now i.e. even if our rejectCallback gets called successfully
+                       //Q:: this is DIFFERENT from our understadning till now i.e. even if our rejectCallback gets called successfully
                        // we still resolve the promise, but in this scenario, we are rejecting it!
+
                        const rejectPromise = (r) => {
                          if(invocationCnt++ > 0) return;
-                         correspondingPromise(null,r);
-                       }
+                         try{
+                             correspondingPromise(null,r);
+                         }catch(error){
+                             correspondingPromise(null,error);
+                         }
+                        }
 
                        //Q: how to ignore multiple calls to them?  inside them?
-                       //A::oncify them using closures?
+                       //A: oncify them using closures?
 
-                       correspondingPromise(thenProperty(resolvePromise,rejectPromise),null);// this will call resolve method for this promise
-                       // and it will resolve it acc. on same basis i.e. if it's a simple value, another promise or thenable
-                    
+                       //ToDo : To ignore, when calling thenproperty throws ex and if resolvePromise/rejectPromise have already been called?
+                       //done
+
+                       thenProperty(resolvePromise,rejectPromise);
+                      //Q: We have defined it but how to call it with values??? Not clear in SPEC
+                      //thenProperty(resolvePromise(this.value),rejectPromise(this.reason)) this we we are calling both????
+                      //A: I guess I got it. We are calling it now with the reqd fn defs,which will be called by it's own code acc.
+
                     }
                     else correspondingPromise(result,null);
                 }
-                // **** OUT OF SPEC  but again a valid case! ****
+                // **** OUT OF SPEC  but again a valid case if it has no then property! ****
                 else correspondingPromise(result,null);
             }
             // if it's a simple value, pass the value directly for it to resolve with that
@@ -103,7 +116,7 @@ class TestPromise{
             correspondingPromise(null,error);
           }finally{
              //cleanup of all registered callbacks and promises
-             this.onResolveCallbacks.shift();
+             this.onFulfilledCallbacks.shift();
              this.registeredPromises.shift();
           }
       }
@@ -114,32 +127,32 @@ class TestPromise{
     reject(rsn){
        //if the promise is already resolved/rejected & we have the corresponding value & reason also set
       //ignore this invocation
-      if((this.status=='RESOLVED' && this.value) || (this.status=='REJECTED' && this.reason)) return;
+      if((this.status=='FULFILLED' && this.value) || (this.status=='REJECTED' && this.reason)) return;
       
       //else set the status and reason accordingly & continue for it's own reg. callbacks
       this.status='REJECTED';
       this.reason = rsn;
 
-      for(let i=0; i<this.onRejectCallbacks.length(); i++){
+      for(let i=0; i<this.onRejectedCallbacks.length(); i++){
           
-          let currentRejectCallback = this.onRejectCallbacks[i];
+          let currentRejectCallback = this.onRejectedCallbacks[i];
           let correspondingPromise = this.registeredPromises[i];
           
           if(!currentRejectCallback || typeof currentRejectCallback !='function') {
             correspondingPromise(null,rsn); // <-- reject the corresponding promise with reason directly
-            return;
+            continue;
           };
 
           try{
             
             //ToDo:: to wait until execution stack is empty!!
+            
             //Invoke the callback with the rsn param received
-
             let result = currentRejectCallback(rsn);
+            
             //Q: can rejection result further be a Promise or thenable?
             //A: Although doesn't sounds much apt, but even if it is, it will get resolved using the Promise resolution procedure
-            //as we are resolving the promise even if our rejection callback executed successfully.
-
+            //as we are resolving the promise even if our rejection callback executed successfull
             correspondingPromise(result,null);
            
           }catch(error){
@@ -147,19 +160,19 @@ class TestPromise{
           }finally{
              
              //cleanup of registered callbacks and promises
-             this.onRejectCallbacks.shift(); // or splice(1,i)??
+             this.onRejectedCallbacks.shift(); // or splice(1,i)??
              this.registeredPromises.shift();
           }
       }
     }
 
-    then(onResolve, onReject){
+    then(onFulfilled, onRejected){
   
-      if(onResolve && typeof onResolve == 'function') this.onResolveCallbacks.push(onResolve);
-      else this.onResolveCallbacks.push(null); // to keep the array's indexes in sync esp w.r.t promises array
+      if(onFulfilled && typeof onFulfilled == 'function') this.onFulfilledCallbacks.push(onFulfilled);
+      else this.onFulfilledCallbacks.push(null); // to keep the array's indexes in sync esp w.r.t promises array
 
-      if(onReject && typeof onReject == 'function') this.onRejectCallbacks.push(onReject);
-      else this.onRejectCallbacks.push(null);
+      if(onRejected && typeof onRejected == 'function') this.onRejectedCallbacks.push(onRejected);
+      else this.onRejectedCallbacks.push(null);
 
       // Where the Magic happens!
       //It's created as a function, so that it can be invoked ON-DEMAND with the values
@@ -169,48 +182,48 @@ class TestPromise{
       })
       this.registeredPromises.push(newPromise);
 
-      //i.e. our promise has already resolved/rejected & we can entertain it immediately
+      //i.e. IF our promise has already fulfilled/rejected & we can entertain it immediately
       //based on our value/reason   
       if(status!='PENDING') {
             
         try{
-            //ToDo:: to call onResolve/onReject only when execution call stack contains platform code
+            //ToDo:: to call onFulfilled/onRejected only when execution call stack contains platform code
             //Approach : By submitting it in microtask queue?
 
-            //ToDo: onResolve/onReject must be called as functions without 'this'
+            //ToDo: onFulfilled/onRejected must be called as functions without 'this'
             //Approach : using strict as first line? also what about the this I'm using here for the sake of clarity?
 
             let x;
 
             if(this.value) {
-              if(!onResolve || typeof onResolve == 'function') x = onResolve(this.value);
+              if(!onFulfilled || typeof onFulfilled == 'function') x = onFulfilled(this.value);
               else x= this.value;
               
-              newPromise(x,null); // <-- this will run it's own resolve() aka 'promise resolution procedure' afterwards
+              newPromise(x,null); // <-- this will run it's own resolve() i.e.'promise resolution procedure' accordingly
             }
             else {
-              if(!onReject || typeof onReject == 'function') {
-                x = onReject(this.reason);
-                //if our original promise got rejected and our 'onReject' callback provided was a valid callback
+              if(!onRejected || typeof onRejected == 'function') {
+                x = onRejected(this.reason);
+                //if our original promise got rejected and our 'onRejected' callback provided was a valid callback
                 //we resolve the new promise.
                 newPromise(x,null);
               }else{
-                //if our original promise got rejected and our 'onReject' callback provided as argument was invalid
+                //if our original promise got rejected and our 'onRejected' callback provided as argument was invalid
                 //we reject the new promise.
                 newPromise(null,this.reason);
               }
            }
-            // It's like saying to the root promise: "hey, these are my onResolve/OnReject callbacks in case you 
-            // happen to get resolve or reject & any of my callback gets executed, I'm resolved" 
+            // It's like saying to the root promise: "hey, these are my onFulfilled/OnRejected callbacks in case you 
+            // happen to get fulfilled or rejected & any of my callback gets executed successfully, I'm fulfilled." 
 
         }catch(error){
-          //no point of doing onReject(error) again, simply pass the error so that it get's rejected accordingly.
+          //no point of doing onRejected(error) again, simply pass the error so that it get's rejected accordingly.
           newPromise(null,error);
         
         }finally{
           //cleanup the latest added array elements
-          this.onResolveCallbacks.pop();
-          this.onRejectCallbacks.pop();
+          this.onFulfilledCallbacks.pop();
+          this.onRejectedCallbacks.pop();
           this.registeredPromises.pop();
         }
       }
@@ -218,7 +231,7 @@ class TestPromise{
     return newPromise;
   }
 
-  //Test suite Adapter requirements
+  //Test suite Adapter requirements(currently based on assumption,will be verified during tests)
   resolved(value){
     return new TestPromise((resolve,reject) => resolve(value));
   }
@@ -235,8 +248,8 @@ class TestPromise{
     
     return {
         'promise': newPromise,
-        'resolve': this.resolve(this.value),
-        'reject': this.reject(this.reason)
+        'resolve': this.resolve,
+        'reject': this.reject
     }
   }
 }
