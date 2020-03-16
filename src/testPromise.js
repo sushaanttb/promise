8fcff1d4fc1371e6b0bde09792ee0ae310b39099
Sushant <sushantbhalla@ymail.com>
2020-03-16 16:46:21 +0530

    Bug Fixes. 90% Test cases passed
    Bug fix : incorrect if check
    2.2.7 : bug fix: no genericdeduce() in reject,to be rejected directly.
    	bug fix: when onRejected & onResolve is strictly a function(should not be any object)

--- src/testPromise.js
+++ src/testPromise.js
@@ -11,125 +11,124 @@ module.exports = class TestPromise{
     onRejectedCallbacks = new Array();
     registeredPromises = new Array();
 
-    //where f is a function with 2 args : resolve/reject
-    //def. of how resolve/reject works will be provided from client
-    //ToDo:: need to confirm the syntax here,should be inline & also the constructor body if reqd.
+  
     constructor(resolver){
-      if(!resolver || typeof resolver != 'function') this.reject(TypeError("Promise resolver is not a function!"));
-      // console.log("In constructor, this::"+JSON.stringify(this));
-      // resolver.bind(this,this.resolve,this.reject);
-      
-      // var newResolver=resolver.bind(this);
-      // newResolver(this.resolve,this.reject);
-
+      if(!resolver || typeof resolver != 'function') this.reject(new TypeError("Promise resolver is not a function!"));
       resolver(this.resolve, this.reject);
-      
     }
 
-  genericDeduce=(x,resolutionType)=>{
-      //From spec 2.3, as per promise resolution procedure 
-      // console.log("genericDeduce called with x:: " + x + "  resolutionType::"+resolutionType);
-      try{
-              if(!x) this.updatePromiseStatus(x,resolutionType); 
-              else if(x instanceof TestPromise) x.then((v)=>this.resolve(v), (r) => this.reject(r)); //<-- adopting it's state
-              else if(typeof x === 'object'){
+    genericDeduce=(x,resolutionType)=>{
+        //From spec 2.3, as per promise resolution procedure 
+        try{
+                // console.log("x:::"+JSON.stringify(x)+ ","+typeof(x));
+                if(!x){ //console.log("In 1st ..."); 
+                this.updatePromiseStatus(x,resolutionType); }
+                else if(x instanceof TestPromise) x.then((v)=>this.resolve(v), (r) => this.reject(r)); //<-- adopting it's state
+                else if(typeof x === 'object'){
+                  // console.log("In 3rd ...");
+                  // console.log(Object.getOwnPropertyNames(x));
+                  
+                    if(Object.prototype.hasOwnProperty.call(x, 'then')){
+                        let thenProperty = x.then;
 
-                // console.log("In genericDeduce when x is object x:: " + x );
-                // console.log("In genericDeduce when x is object x:: "+x+" has then?:: " + x.hasOwnProperty('then'));
-                
-                   if(x.hasOwnProperty('then')){
-                      let thenProperty = x.then;
+                        // console.log("In genericDeduce thenProperty:: "+thenProperty);
+                  
+                        if(typeof thenProperty === 'function'){
+                            //ToDo: to call it with this=x
+                            //A: Although I believe it should be fine since by def. of it ,it is already val.then 
 
-                      if(typeof thenProperty === 'function'){
-                          //ToDo: to call it with this=x
-                          //A: Although I believe it should be fine since by def. of it ,it is already val.then 
+                          let invocationCnt = 0;
+                          const resolvePromise = (y) => {
+                            
+                            if(invocationCnt++ >0) return; //oncifying attempt using closure : NEEDS to be tested.   
+                            
+                            // console.log("In oncified resolvePromise,invocationCnt::"+invocationCnt);
+                                try{
+                                  this.resolve(y); // <-- fulfill
+                                }catch(error){
+                                  this.reject(error); // <-- reject with error
+                                }
+                            }
+                          const rejectPromise = (r) => {
 
-                         let invocationCnt = 0;
-                         const resolvePromise = (y) => {
-                           if(invocationCnt++ >0) return; //oncifying attempt using closure : NEEDS to be tested.    
-                               try{
-                                 this.resolve(y); // <-- fulfill
-                               }catch(error){
-                                 this.reject(error); // <-- reject with error
-                               }
-                          }
-                         const rejectPromise = (r) => {
-                           if(invocationCnt++ >0) return; //oncifying attempt using closure : NEEDS to be tested.
-                             try{
-                                 this.reject(r); // <-- reject with reason
-                             }catch(error){
-                                 this.reject(error); // <-- reject with error
-                             }
-                          }
-                          // TODO: when resolvePromise & rejectPromise are called at same time,ignored 2nd invocation
-                         // A: although I believe the above oncify attempt & also the status checks in resolve/reject should do the
-                         // trick, still there can be a race condition and it would be great if they can work on a common
-                         // invocationCnt variable.
-                         
-                        thenProperty(resolvePromise,rejectPromise);// <-- calling then function with the above created oncified functions
+                            if(invocationCnt++ >0) return; //oncifying attempt using closure : NEEDS to be tested.
+
+                            // console.log("In oncified rejectPromise,invocationCnt::"+invocationCnt);
+
+                              try{
+                                  this.reject(r); // <-- reject with reason
+                              }catch(error){
+                                  this.reject(error); // <-- reject with error
+                              }
+                            }
+                            // TODO: when resolvePromise & rejectPromise are called at same time,ignored 2nd invocation
+                          // A: although I believe the above oncify attempt & also the status checks in resolve/reject should do the
+                          // trick, still there can be a race condition and it would be great if they can work on a common
+                          // invocationCnt variable.
                           
-                      }else{ // i.e. if it has a then property but it is not a function
-                        this.updatePromiseStatus(x,resolutionType);
-                      }
-                   }else{// **** OUT OF SPEC  but a valid case if it has no then property! ****
-                     this.updatePromiseStatus(x,resolutionType);
-                   }       
-              }
-              else{
-                this.updatePromiseStatus(x,resolutionType);     
-              }
-      }catch(error){
-          this.reject(error);
-      }
-  }
-
-  updatePromiseStatus=(x,resolutionType)=>{
-    
-    if(this.status=='FULFILLED' || this.status=='REJECTED') return;
-      
-    if(resolutionType==='fulfill'){
-       this.status='FULFILLED';
-       this.value = x;
-    }else{
-       this.status='REJECTED';
-       this.reason = x;
+                          thenProperty(resolvePromise,rejectPromise);// <-- calling then function with the above created oncified functions
+                            
+                        }else{ // i.e. if it has a then property but it is not a function
+                          this.updatePromiseStatus(x,resolutionType);
+                        }
+                    }else{// **** OUT OF SPEC  but a valid case if it has no then property! ****
+                      this.updatePromiseStatus(x,resolutionType);
+                    }       
+                }
+                else this.updatePromiseStatus(x,resolutionType);     
+                
+        }catch(error){
+            this.reject(error);
+        }
     }
 
-    // console.log("In updatePromiseStatus, (x,resolutionType):: ("+ JSON.stringify(x)+ ", "+resolutionType+")");
-  }
-
-  resolveCallbacks=()=>{
-       for(let i=0; i<this.deferreds.length; i++){
+    updatePromiseStatus=(x,resolutionType)=>{
       
-            // let correspondingPromise = registeredPromises[i];
-            let deferred = this.deferreds[i];
-            let currentFulfillCallback = this.onFulfilledCallbacks[i];
-            let currentRejectCallback = this.onRejectedCallbacks[i];
-         
-            try{
-              
-                  if(this.status=='FULFILLED' && (!currentFulfillCallback || typeof currentFulfillCallback != 'function')) {
-                        deferred.resolve(this.value);// <-- fulfill the corresponding promise with the value directly(2.2.7.3)
-                  }else if(this.status=='REJECTED' && (!currentRejectCallback || typeof currentRejectCallback !='function')) {
-                        deferred.reject(this.reason); // <-- reject the corresponding promise with reason directly(2.2.7.4)
-                  }else{
-                    //ToDo:: When invoking any callback: to wait until execution stack is empty!!
-                    //Approach : by submitting it in using microtaskqueue?
-                    let result;
-                    this.status=='FULFILLED'? (result = currentFulfillCallback(this.value)) : (result = currentRejectCallback(this.reason));
-                    deferred.resolve(result);// <-- fulfill the corresponding promise based on it's callback value since callback executed successfully
-                 }
-            }catch(error){
-              deferred.reject(error);// <-- reject if any error
-            }finally{
-              //cleanup of all registered callbacks and promises irrespective of it was fulfilled/rejected
-              this.deferreds.shift();
-              this.onFulfilledCallbacks.shift();
-              this.onRejectedCallbacks.shift();
-              this.registeredPromises.shift();
-            }
-         }
-  }
+      if(this.status=='FULFILLED' || this.status=='REJECTED') return;
+        
+      if(resolutionType==='fulfill'){
+        this.status='FULFILLED';
+        this.value = x;
+      }else{
+        this.status='REJECTED';
+        this.reason = x;
+      }
+
+      // console.log("In updatePromiseStatus, (x,resolutionType):: ("+ JSON.stringify(x)+ ", "+resolutionType+")");
+    }
+
+    resolveCallbacks=()=>{
+        for(let i=0; i<this.deferreds.length; i++){
+        
+              // let correspondingPromise = registeredPromises[i];
+              let deferred = this.deferreds[i];
+              let currentFulfillCallback = this.onFulfilledCallbacks[i];
+              let currentRejectCallback = this.onRejectedCallbacks[i];
+          
+              try{
+                
+                    if(this.status=='FULFILLED' && (!currentFulfillCallback || typeof currentFulfillCallback != 'function')) {
+                          deferred.resolve(this.value);// <-- fulfill the corresponding promise with the value directly(2.2.7.3)
+                    }else if(this.status=='REJECTED' && (!currentRejectCallback || typeof currentRejectCallback !='function')) {
+                          deferred.reject(this.reason); // <-- reject the corresponding promise with reason directly(2.2.7.4)
+                    }else{
+                      //ToDo:: When invoking any callback: to wait until execution stack is empty!!
+                      //Approach : by submitting it in using microtaskqueue?
+                      let result;
+                      this.status=='FULFILLED'? (result = currentFulfillCallback(this.value)) : (result = currentRejectCallback(this.reason));
+                      deferred.resolve(result);// <-- fulfill the corresponding promise based on it's callback value since callback executed successfully
+                  }
+              }catch(error){
+                deferred.reject(error);// <-- reject if any error
+              }finally{
+                //cleanup of all registered callbacks and promises irrespective of it was fulfilled/rejected
+                this.deferreds.shift();
+                this.onFulfilledCallbacks.shift();
+                this.onRejectedCallbacks.shift();
+                this.registeredPromises.shift();
+              }
+          }
+    }
 
     //ToDo:: to make it private
     resolve=(val)=>{
@@ -137,7 +136,7 @@ module.exports = class TestPromise{
       // console.log("resolve called with::"+val);
       
       if (this.status=='FULFILLED') return;
-      if (val===this) return this.reject(TypeError("resolution value can't be the same promise!")); //(From the Spec,2.3.1)
+      if (val===this) return this.reject(new TypeError("resolution value can't be the same promise!")); //(From the Spec,2.3.1)
 
       this.genericDeduce(val,'fulfill');
       
@@ -152,12 +151,13 @@ module.exports = class TestPromise{
       // console.log("reject called with::"+JSON.stringify(rsn));
 
       if(this.status=='REJECTED') return;
-      if (rsn===this) return this.reject(TypeError("rejection reason can't be the same promise!")); //(From the Spec ,2.3.1)
-      
-      // this.status = 'REJECTED';
-      // this.reason = rsn; //Q: do we need same promise/thenable checks? A: yes
+      if (rsn===this) return this.reject(new TypeError("rejection reason can't be the same promise!")); //(From the Spec ,2.3.1)
+      // console.log("In reject, is rsn typerror::"+ rsn instanceof TypeError);
 
-      this.genericDeduce(rsn,'reject');
+      this.status = 'REJECTED';
+      this.reason = rsn; //Q: do we need same promise/thenable checks? A: yes
+
+      // this.genericDeduce(rsn,'reject');
       
       //since this.reason can be undefined
       if(this.status=='REJECTED') this.resolveCallbacks();
@@ -202,19 +202,26 @@ module.exports = class TestPromise{
             // console.log("In then(),when root promise is already Resolved status:: "+ this.status);
 
             if(this.status=='FULFILLED') { //since value can be undefined
-              // console.log("In then(),value when root promise is FULFILLED value:: "+ JSON.stringify(this.value) + " isNull?:: "+(this.value==null));
+              // console.log("In then(),value when root promise is FULFILLED value:: "+ JSON.stringify(this.value));
 
-              if(!onFulfilled || typeof onFulfilled == 'function') x = onFulfilled(this.value);
+              // console.log("In then(),when root promise is FULFILLED onFulfilled:: "+ onFulfilled);
+             
+              if(typeof onFulfilled === 'function') x = onFulfilled(this.value);
               else x= this.value;
 
-              // console.log("In then(),x when root promise is FULFILLED x:: "+ x);
+              // if(x==d)  return d.reject(new TypeError("resolution value can't be the same promise!"));
+
+              // console.log("In then(),when root promise is FULFILLED x:: "+ x);
               
               d.resolve(x); // <-- this will run resolve() of newPromise
             }
             else {
-              if(!onRejected || typeof onRejected == 'function') {
+              
+              if(typeof onRejected === 'function') {
                 x = onRejected(this.reason);
-                
+
+                // if(x==d)  return d.reject(new TypeError(this.reason));
+
                 //From 2.2.7.1:: i.e if our root promise got rejected and the 'onRejected' callback was valid callback
                 // we resolve the new promise.
                 d.resolve(x);// <-- this will run resolve() of newPromise
